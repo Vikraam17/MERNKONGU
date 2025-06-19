0a51fdbed0503ccf021b7a0f9fd6a5fb1c7ad163
Viky <vikraam17@gmail.com>
2025-06-19 16:27:55 +0530

    Day3

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -60,3 +60,14 @@ console.log(total)
 
 var result = arr.map(num => num * 2).filter(num => num > 5).reduce((val, num) => val + num, 0)
 console.log(result)
+
+var choice = ['rock','paper','scissor'];
+var computerChoice = choice[Math.floor(Math.random()*3)];
+console.log(computerChoice)
+
+var details =async () =>{
+    var responce = await fetch('https://dummyjson.com/users')
+    var users  = await responce.json();
+    console.log(users)
+}
+details()
--- React/src/App.jsx
+++ React/src/App.jsx
@@ -1,13 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
+import Child from './components/Child'
+import Home from './Pages/Home'
+import About from './Pages/About'
+import Contact from './Pages/Contact'
+import Skills from './Pages/Skills'
+import Counter from './components/Counter'
 
-function App() {
-
+const App = () => {
+  var fruits = ["Apple","Banana","Orange"];
+  var user = {username:"viky",password:"123456"}
   return (
     <div>
-      <h1>Hello world</h1>
+      {/* <Child name="viky" phnNum="9876543210" Dept="IT"/>
+      <Child name="Muthu" phnNum="9876543210" Dept="IT"/>
+      <Home items = {fruits} users={user}/>
+      <About/>
+      <Contact/>
+      <Skills/> */}
+      <Counter/>
     </div>
   )
 }
